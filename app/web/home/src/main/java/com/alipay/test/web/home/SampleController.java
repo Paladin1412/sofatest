7f5b9f46f4633783b3bafba4373e842afcc8b58a
unknown <songlovecr@gmail.com>
2020-06-18 10:12:45 +0800

    get config

--- app/web/home/src/main/java/com/alipay/test/web/home/SampleController.java
+++ app/web/home/src/main/java/com/alipay/test/web/home/SampleController.java
@@ -4,6 +4,8 @@
  */
 package com.alipay.test.web.home;
 
+import com.alipay.sofa.runtime.api.aware.AppConfigurationAware;
+import com.alipay.sofa.runtime.api.component.AppConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,22 @@ import org.springframework.ui.ModelMap;
  * A sample controller.
  */
 @Controller
-public class SampleController {
+public class SampleController implements AppConfigurationAware {
 
 	@Autowired
 	private SpringBean springBean;
 
+	private AppConfiguration appConfiguration;
+
 	@RequestMapping(value="/sample",method = RequestMethod.GET)
 	public void doGet(ModelMap modelMap) {
 
-       modelMap.put("hello",springBean.getWord());
+       modelMap.put("hello",appConfiguration.getPropertyValue("config"));
 
+	}
+
+	@Override
+	public void setAppConfiguration(AppConfiguration appConfiguration) {
+		this.appConfiguration = appConfiguration;
 	}
 }
